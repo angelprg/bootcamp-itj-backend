919fd4b04e17f0f598c6daa92215b1ffb84cac0b
Isaac Cisneros <isaac.cisneros@itjuana.com>
2022-03-05 13:13:56 -0800

    session-03

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const postsRouter = require("./src/routes/posts");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 const PORT = 4000;
 
 app.use(express.json());
-app.use("/posts", postsRouter)
+app.use("/posts", postsRouter);
+// app.use((error, req, res, next) => {
+//   console.log("MIDDLEWARE FUNCTION!");
+//   console.error(error);
+//   res.status(500).json(error);
+// });
+
+const connectDb = () => {
+  try {
+    mongoose.connect(process.env.DB_URI);
+    console.log("Database connected");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+console.log(process.env.DB_URI);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  connectDb();
 });
