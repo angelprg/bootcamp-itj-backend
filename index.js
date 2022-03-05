78991492546720d0573b480d54ac71ea1d10731f
Alfredo Alcántara <alfredo.alcantara.r@gmail.com>
2022-03-05 13:44:21 -0800

    Adds cors library

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors")
 const postsRouter = require("./src/routes/posts");
 const mongoose = require("mongoose");
 require("dotenv").config();
@@ -7,6 +8,7 @@ const app = express();
 const PORT = 4000;
 
 app.use(express.json());
+app.use(cors());
 app.use("/posts", postsRouter);
 // app.use((error, req, res, next) => {
 //   console.log("MIDDLEWARE FUNCTION!");
